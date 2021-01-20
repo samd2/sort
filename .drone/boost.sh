43525fefc479eea5bc9cf4bb7ea453d5b741993e
sdarwin <samuel.d.darwin@gmail.com>
2021-01-20 15:45:36 +0000

    add drone config

--- .drone/boost.sh
+++ .drone/boost.sh
@@ -4,7 +4,7 @@
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
 
-set -e
+set -ex
 export TRAVIS_BUILD_DIR=$(pwd)
 export DRONE_BUILD_DIR=$(pwd)
 export TRAVIS_BRANCH=$DRONE_BRANCH
@@ -12,8 +12,6 @@ export VCS_COMMIT_ID=$DRONE_COMMIT
 export GIT_COMMIT=$DRONE_COMMIT
 export REPO_NAME=$DRONE_REPO
 export PATH=~/.local/bin:/usr/local/bin:$PATH
-
-if [ "$DRONE_JOB_BUILDTYPE" == "boost" ]; then
 
 echo '==================================> INSTALL'
 
@@ -32,5 +30,3 @@ python tools/boostdep/depinst/depinst.py sort
 echo '==================================> SCRIPT'
 
 ./b2 libs/sort/test toolset=$TOOLSET cxxstd=$CXXSTD
-
-fi
